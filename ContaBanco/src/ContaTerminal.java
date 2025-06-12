7acf56ea46ece7fe095b87cd1d8878eefcb62893
locksher182 <182anderton@gmail.com>
2025-06-11 21:11:34 -0300

    Final sem linhas de comentários.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-        //TODO: Conhecer e importar a Classe Scanner
-
-        //Exibir as mensagens para o usuário
+        
         var scanner = new Scanner(System.in);
         System.out.println("Por favor, digite o número da Agência: ");
         String agency = scanner.next();
@@ -18,9 +16,6 @@ public class ContaTerminal {
         BigDecimal deposit = scanner.nextBigDecimal();
 
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo R$ %.2f já está disponível para saque", clientName, agency, account, deposit);
-        //Obter pela classe scanner os valores digitados no terminal
-        //Exibir a mensagem final
-        //"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], 
-        //conta [Numero] e seu saldo [Saldo] já está disponível para saque".
+       
     }
 }
